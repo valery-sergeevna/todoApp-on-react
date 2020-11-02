aa18a7081abd20b6a19c30f89ad652762392240e
Valery <lekalera777@gmail.com>
2020-11-02 21:04:25 +0200

    Updates

---
+++ precache-manifest.41d8d1b477596070db2389cfa312dd9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7738fa24e5a81bdf62b1a2e992dd9bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "546448a7c72cfcb3d9e0",
+    "url": "/static/css/main.87b2a6f8.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "546448a7c72cfcb3d9e0",
+    "url": "/static/js/main.f69bb2bd.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
